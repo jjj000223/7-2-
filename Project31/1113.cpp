10f7d2016e7bde92a8257088b663f128ad046b5b
지연 이 <jjj000223@daum.net>
2019-11-13 15:07:30 +0900

    초기 커밋.

---
+++ Project31/1113.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int i, j;
+	for (i = 1; i <= 5; i++)
+	{
+		for (j = 5; j >= 1; j--)
+		{
+			if (i < j)
+				printf(" ");
+			else
+				printf("%d", j);
+		}
+		printf("\n");
+	}
+}
